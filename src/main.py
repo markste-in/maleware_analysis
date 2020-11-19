cba89a401ff17b4a16da6e2cba22b79740998fae
markste-in <markstein@Tommys-MBP.fritz.box>
2020-11-19 22:50:02 +0100

    automated json creation

--- src/main.py
+++ src/main.py
@@ -3,21 +3,27 @@ import os
 
 from util.cleanup import iterdict
 from pe_header import analyze_pefile
+from util.json import flatten_json
+from pandas import json_normalize
+import pandas as pd
 
-
+from glob import glob
 
 if __name__ == '__main__':
-    print(os.path.curdir)
-    file = 'samples/lua54.exe' #some simple benign samples
-    if not os.path.exists(file): raise Exception("File does not exist")
+    files = glob('samples/*.exe')
 
-    result = iterdict(analyze_pefile(file)) 
-    with open("samples/results/sample.json", "w") as outfile: 
-        dump = json.dumps(result, sort_keys=False,indent=4, separators=(',', ': '))
-        print(dump)
-        outfile.write(dump)
-    
-
+    for file in files:
+        if not os.path.exists(file): raise Exception("File does not exist")
+        result = iterdict(analyze_pefile(file)) 
+        with open("samples/results/" + result['sha1'] + ".json", "w") as outfile: 
+            dump = json.dumps(result, sort_keys=False,indent=4, separators=(',', ': '))
+            outfile.write(dump)
+        print("... done with", file)
+    # print(dump)
+    # print(pd.DataFrame([flatten_json(result)]))
+    # print(json_normalize([flatten_json(result)]))
+    # print(files)
+    # print(json_normalize([flatten_json(result['pe_info'])]))
 
 
 
---
+++ src/util/json.py
@@ -0,0 +1,17 @@
+def flatten_json(y): #changed version of https://stackoverflow.com/a/51379007
+    out = {}
+
+    def flatten(x, name=''):
+        if isinstance(x, dict):
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif isinstance(x,list) or isinstance(x, tuple):
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
