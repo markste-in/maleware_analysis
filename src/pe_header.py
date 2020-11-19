21713344a5234bd797d5461861ea030207cd7a83
markste-in <markstein@Tommys-MBP.fritz.box>
2020-11-19 19:27:41 +0100

    some more structure and included more files in gitignore

--- src/pe_header.py
+++ src/pe_header.py
@@ -1,6 +1,6 @@
 from util.hashing import hash_file
 import pefile
-from strings import extract_all_strings
+from util.commands import extract_all_strings, filetype
 import os
 
 def analyze_pefile(filename):
@@ -8,6 +8,7 @@ def analyze_pefile(filename):
     out['filename'] = os.path.basename(filename)
     out['md5'] = hash_file(filename,"md5")
     out['sha1'] = hash_file(filename,"sha1")
+    out['filetype'] = filetype(filename)
     try:
         pe = pefile.PE(filename)
         out["pe_info"] = pe.dump_dict()
--- src/util/commands.py
+++ src/util/commands.py
@@ -2,3 +2,7 @@ import subprocess
 def extract_all_strings(file):
     lst=subprocess.check_output(['strings', file]).decode().split('\n')
     return lst
+
+def filetype(file):
+    lst=subprocess.check_output(['file', file]).decode().strip('\n')
+    return lst
